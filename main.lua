46e8b42293eac014d81082b9bb90c96b17d23bfe
Joel Singh <joelsingh788@gmail.com>
2024-05-30 12:58:27 -0400

    10 by 10 grid of letters generated

--- main.lua
+++ main.lua
@@ -1,10 +1,34 @@
-local board = {"a", "b", "c", "d", "e", "f"}
+local board = {}
+
+local function getInitialBoard()
+  local function create10Letters()
+    return {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j"}
+  end
+  return {
+    create10Letters(),
+    create10Letters(),
+    create10Letters(),
+    create10Letters(),
+    create10Letters(),
+    create10Letters(),
+    create10Letters(),
+    create10Letters(),
+    create10Letters(),
+    create10Letters(),
+  }
+end
 
 function love.load()
-  local SpaceMono = love.graphics.newFont("SpaceMono-Regular.ttf")
-  love.graphics.setFont(SpaceMono)
+  board = getInitialBoard()
+  local Font = love.graphics.newFont(16)
+  love.graphics.setFont(Font)
+
 end
 
 function love.draw()
-  love.graphics.print("Hello World!", 100, 200)
+  for indexRow,row in ipairs(board) do
+    for indexColumn,letter in ipairs(row) do
+      love.graphics.print(letter, indexColumn * 30, indexRow * 30)
+    end
+  end
 end
