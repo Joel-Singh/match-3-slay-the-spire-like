0a7ec2af2d2834e9b7ccb11496d8ff21ab1bb128
Joel Singh <joelsingh788@gmail.com>
2024-06-03 12:40:08 -0400

    Move board into its own file

--- main.lua
+++ main.lua
@@ -1,49 +1,5 @@
+local getInitialBoard = require("board")
 local board = {}
-
-local function getInitialBoard()
-  local function create10letters()
-    return {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j"}
-  end
-  local board = {
-    create10letters(),
-    create10letters(),
-    create10letters(),
-    create10letters(),
-    create10letters(),
-    create10letters(),
-    create10letters(),
-    create10letters(),
-    create10letters(),
-    create10letters(),
-    x = 50,
-    y = 50,
-    width = 300,
-    height = 300,
-  }
-
-  local function countnumerickeys(table)
-    local count = 0
-    for _,_ in ipairs(table) do
-      count = count + 1
-    end
-    return count
-  end
-
-  board.ycount = countnumerickeys(board)
-  board.xcount = countnumerickeys(board[1])
-
-  board.xspacing = board.width / (board.xcount - 1)
-
-  board.yspacing = board.height / (board.ycount - 1)
-
-  local letterHeight = 32
-  board.yspacing = board.yspacing - (letterHeight / board.ycount - 1)
-
-  board.xend = board.x + board.width
-  board.yend = board.x + board.height
-
-  return board
-end
 
 function love.load()
   board = getInitialBoard()
