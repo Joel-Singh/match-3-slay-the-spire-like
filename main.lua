401a4708df8ababe71995d03bfdd7dd615a9499a
Joel Singh <joelsingh788@gmail.com>
2024-06-03 11:24:13 -0400

    Board has spacing associated with it

--- main.lua
+++ main.lua
@@ -4,7 +4,7 @@ local function getInitialBoard()
   local function create10letters()
     return {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j"}
   end
-  return {
+  local board = {
     create10letters(),
     create10letters(),
     create10letters(),
@@ -17,8 +17,16 @@ local function getInitialBoard()
     create10letters(),
     x = 50,
     y = 50,
-    spacing = 30
+    width = 300,
+    height = 300,
+    ycount = 10,
+    xcount = 10,
   }
+
+  board.xspacing = board.width / board.xcount
+  board.yspacing = board.height / board.ycount
+
+  return board
 end
 
 function love.load()
@@ -31,7 +39,12 @@ end
 function love.draw()
   for indexrow,row in ipairs(board) do
     for indexcolumn,letter in ipairs(row) do
-      love.graphics.print(letter, indexcolumn * board.spacing + board.x, indexrow * board.spacing + board.y)
+      love.graphics.print(letter, (indexcolumn - 1) * board.xspacing + board.x, (indexrow - 1) * board.yspacing + board.y)
     end
   end
+end
+
+function love.mousepressed( x, y, button, istouch, presses )
+  print("The x is " .. x)
+  print("The y is " .. y)
 end
